854d083e1f9295c07522810990760e220657845c
Nikita Calmic <nikita.calmic@indrivo.com>
2020-06-25 10:45:30 +0300

    App fix

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -7,23 +7,21 @@ import BoardPage from '../../pages/BoardPage/BoardPage';
 import CardPage from '../../pages/CardPage/CardPage';
 
 const App = () => {
-    return (
-        <>
-            <Header />
-            <Switch>
-                <Route path='/board/:id/list/:listId/card/:cardId'>
-                    <BoardPage />
-                    <CardPage />
-                </Route>
-                <Route path='/board/:id'>
-                    <BoardPage />
-                </Route>
-                <Route path='/'>
-                    <BoardsPage />
-                </Route>
-            </Switch>
-        </>
-    )
+    return <>
+        <Header />
+        <Switch>
+            <Route path='/board/:id/list/:listId/card/:cardId'>
+                <BoardPage />
+                <CardPage />
+            </Route>
+            <Route path='/board/:id'>
+                <BoardPage />
+            </Route>
+            <Route path='/'>
+                <BoardsPage />
+            </Route>
+        </Switch>
+    </>
 }
 
 export default App;
